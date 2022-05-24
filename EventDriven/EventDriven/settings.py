11f2bf6375aa7cc3d00e6d309d287dbd0d9e48f8
olafurp18 <olafurp18@ru.is>
2022-05-24 14:28:17 +0000

    Add database connection

--- EventDriven/EventDriven/settings.py
+++ EventDriven/EventDriven/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'EventDriven.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'event-driven',
+        'USER': 'main-db-user',
+        'PASSWORD': 'mainpassword123',
+        'HOST': '34.142.79.225',
+        'PORT': '5432'
+
     }
 }
 
