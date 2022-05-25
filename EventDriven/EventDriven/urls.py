1fbabc1b01670172261289563c661327dce69eaa
olafurp18 <olafurp18@ru.is>
2022-05-25 15:26:01 +0000

    Fix venue settings

--- EventDriven/EventDriven/urls.py
+++ EventDriven/EventDriven/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', include('event.urls')),
-    path('venues/', include('venue.urls'))
+    #path('venues/', include('venue.urls'))
 ]
--- EventDriven/venue/models.py
+++ EventDriven/venue/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
-    name = models.CharField(255)
+    name = models.CharField(max_length=255)
 
 class Venue(models.Model):
-    name = models.CharField(255)
+    name = models.CharField(max_length=255)
     seating = models.IntegerField()
     accessible_seating = models.IntegerField()
     locationid = models.ForeignKey(Location, on_delete=models.CASCADE)
