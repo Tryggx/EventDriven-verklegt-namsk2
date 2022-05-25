91c31a1c8b6625ccf1d0e3bdf49f0bf583bcae0b
olafurp18 <olafurp18@ru.is>
2022-05-25 13:48:55 +0000

    add Models in event

--- EventDriven/event/models.py
+++ EventDriven/event/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(255)
+    description = models.CharField(4096)
+    poster_image = models.CharField(8192)
+    header_image = models.CharField(8192)
+    #eventttypeid = models.ForeignKey(EventType)
+
+class Show(models.Model):
+    #venueid = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    eventid = models.ForeignKey(Event, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+
+class Zone(models.Model):
+    name = models.CharField(255)
+    showid = models.ForeignKey(Show, on_delete=models.CASCADE)
+    price = models.CharField(100)
