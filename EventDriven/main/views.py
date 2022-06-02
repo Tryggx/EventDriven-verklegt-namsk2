d8030e18ff579bb8a7c47cc5c3ec48e89dc43f8d
olafurp18 <olafurp18@ru.is>
2022-06-02 18:07:05 +0000

    Disable drawing eventType if no events exists with it

--- EventDriven/main/views.py
+++ EventDriven/main/views.py
@@ -12,17 +12,24 @@ from random import randrange
 
 def index(request):
     headliners = Event.objects.filter(headliner=True, show__datetime__gte=datetime.today())
+    events = Event.objects.filter(show__datetime__gte=datetime.today()).distinct()
     if headliners.count() != 0:
         headliners_rnd = randrange(headliners.count())
     else:
         headliners = [False]
         headliners_rnd = 0
     return render(request, 'main/front.html', context={
-        'events': Event.objects.filter(show__datetime__gte=datetime.today()),
-        'eventtypes': EventType.objects.all(),
+        'events': events,
+        'eventtypes': EventType.objects.filter(event__in=events).distinct(),
         'headliners': headliners[headliners_rnd],
         'shows': Show.objects.values().annotate(
             availabletickets=SubqueryAggregate(
                 'zone__total_tickets', filter=Q(showid=OuterRef('id')), aggregate=Sum)-Count('ticket')),
         'zones': Zone.objects.values().annotate(availabletickets=F('total_tickets')-Count('ticket'))
     })
+
+#clock = 7
+#for i in range(10):
+#    NewShow = Show(venueid=Venue.objects.get(pk=3), eventid=Event.objects.get(pk=3), datetime=datetime(2022, 6, 6, clock, 0), seating=1000, accessible_seating=10)
+#    NewShow.save()
+#    clock = clock+1
