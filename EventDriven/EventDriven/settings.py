b352bd8a5f8398469d54e082b5e9d6033d453304
olafurp18 <olafurp18@ru.is>
2022-06-03 17:41:19 +0000

    django secret key to gitignore

--- EventDriven/EventDriven/settings.py
+++ EventDriven/EventDriven/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from secret import SECRET_KEY
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eu809*msg5=m6kv_e%$j*ul!()!li4sg+c3s!pyawdy$^7*z$!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
